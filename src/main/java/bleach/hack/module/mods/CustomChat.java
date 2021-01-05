024b3c0e13ead93bb11d0c82f09f8464380b0c96
BruzzTheHuzz <71281295+BruzzTheHuzz0@users.noreply.github.com>
2021-01-05 12:14:08 -0500

    Update CustomChat.java

--- src/main/java/bleach/hack/module/mods/CustomChat.java
+++ src/main/java/bleach/hack/module/mods/CustomChat.java
@@ -57,7 +57,7 @@ public class CustomChat extends Module {
                     CharMap.single('X', '\u03c7'), CharMap.single('Y', '\u0443')));
 
     public String prefix = "";
-    public String suffix = " | Return Monkey with ApeHack";
+    public String suffix = " <-- don't listen to this message, i'm actually a retard";
 
     public CustomChat() {
         super("CustomChat", KEY_UNBOUND, Category.CHAT, "Customizes your chat messages, use the \"customchat\" command to edit the stuff",
